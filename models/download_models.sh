9b77a34a11606af9171e0169e185b16c9b15465f
Abhishek Das <das.abhshk@gmail.com>
2016-05-27 17:57:19 -0400

    Adds VQA pretrained model and extracted features download script

--- models/download_models.sh
+++ models/download_models.sh
@@ -14,9 +14,19 @@ wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_l
 # wget -c https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/bb2b4fe0a9bb0669211cf3d0bc949dfdda173e9e/VGG_ILSVRC_19_layers_deploy.prototxt
 # wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel
 
-# neuraltalk2 image captioning
-cd ../neuraltalk2
+cd ..
+
+# VQA
+
+cd VQA_LSTM_CNN
+wget -c https://filebox.ece.vt.edu/~jiasenlu/codeRelease/vqaRelease/train_only/data_train_val.zip
+wget -c https://filebox.ece.vt.edu/~jiasenlu/codeRelease/vqaRelease/train_only/pretrained_lstm_train_val.t7.zip
+unzip data_train_val.zip
+unzip pretrained_lstm_train_val.t7.zip
+cd ..
+
+# neuraltalk2
+cd neuraltalk2
 wget http://cs.stanford.edu/people/karpathy/neuraltalk2/checkpoint_v1.zip
 unzip checkpoint_v1.zip
-
 cd ..
